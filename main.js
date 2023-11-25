557da0f6d5d143931b7a946d890d46869653bac2
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-11-25 13:30:05 +0800

    CORS proxy ain't proxying
    It still just goes to the duckduckgo which, as we established, doesn't like CORS

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-/**
- * Finds websites from a top-level domain (TLD).
- *
- * @param {string} TLD - The top-level domain to search for.
- * @returns {Promise<string[]>} - A promise that resolves to an array of website URLs.
- */
 async function findWebsitesFromTLD(TLD) {
-    const searchURL = 'https://corsproxy.io/?' + encodeURIComponent(`https://html.duckduckgo.com/html/?q=site:www.${TLD}`); // apparantly duckduckgo is unhappy with cors
-    console.log('Searching URL:', searchURL);
-
+    const searchURL = `https://corsproxy.io/?${encodeURIComponent(`https://html.duckduckgo.com/html/?q=site:www.${TLD}`)}`;
     const response = await fetch(searchURL);
-    console.log('Response:', response);
-
     const html = await response.text();
-    console.log('HTML:', html);
-
-    const urlRegex = /<a class="result__url" href="([^"]+)">/g;
-    const results = Array.from(html.matchAll(urlRegex), match => match[1].split('/')[0]);
-
-    console.log('Results:', results);
+    const results = html.match(/<a class="result__url" href="([^"]+)">/g)
+        .map((match) => match.match(/<a class="result__url" href="([^"]+)">/)[1].split('/')[0]);
     return results;
 }
