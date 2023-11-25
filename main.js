a39a9266fba7712e89b2df196fbd144e2a92a548
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-11-25 13:12:53 +0800

    Basic logic (should work)

---
+++ main.js
@@ -0,0 +1,22 @@
+/**
+ * Finds websites from a top-level domain (TLD).
+ *
+ * @param {string} TLD - The top-level domain to search for.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of website URLs.
+ */
+async function findWebsitesFromTLD(TLD) {
+    const searchURL = `https://html.duckduckgo.com/html/?q=site:www.${TLD}`;
+    console.log('Searching URL:', searchURL);
+
+    const response = await fetch(searchURL);
+    console.log('Response:', response);
+
+    const html = await response.text();
+    console.log('HTML:', html);
+
+    const urlRegex = /<a class="result__url" href="([^"]+)">/g;
+    const results = Array.from(html.matchAll(urlRegex), match => match[1].split('/')[0]);
+
+    console.log('Results:', results);
+    return results;
+}
